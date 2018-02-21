e63f53aca509a9654525924dd7a3d686bc476a67
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-02-21 10:28:00 +0300

    updated packages

--- src/Lykke.Service.BlockchainBalances.Core/Settings/ClientSettings/NinjaServcieSettings.cs
+++ src/Lykke.Service.BlockchainBalances.Core/Settings/ClientSettings/NinjaServcieSettings.cs
@@ -1,7 +1,10 @@
-﻿namespace Lykke.Service.BlockchainBalances.Core.Settings.ClientSettings
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Service.BlockchainBalances.Core.Settings.ClientSettings
 {
     public class NinjaServcieSettings
     {
+        [HttpCheck("")]
         public string ServiceUrl { get; set; }
     }
 }
